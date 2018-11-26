cebf62058059e49f4dbe72069c164d760a2d3673
Alex Zvorygin <grafetu@gmail.com>
2018-11-26 02:28:03 -0500

    can create arbitrary rectangles

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const cubeEdgeLength  = 2
 const cubeEdgeLengthEm = emString(cubeEdgeLength)
 
 
-const getTransformForFace = (faceIndex, edgeLength) => {
+const getTransformForCubeFace = (faceIndex, edgeLength) => {
   const rotations = [
     'rotateY(0deg)',
     'rotateY(90deg)',
@@ -39,14 +39,14 @@ const createCubeParams = (edgeLength, leftPct, topPct) => {
     face.id = `face-${faceIndex}-${id}`
     container.appendChild(face)
     face.style.backgroundColor = 'black'
-    face.style.margin= -0.5*edgeLengthEm;
+    // face.style.margin= -0.5*edgeLengthEm;
     face.style.width = edgeLengthEm;
     face.style.height = edgeLengthEm;
     face.style.position = 'absolute'
     face.classList.add('wireframe')
     
-    // console.log(getTransformForFace(faceIndex, edgeLength))
-    face.style.transform = getTransformForFace(faceIndex, edgeLength)
+    // console.log(getTransformForCubeFace(faceIndex, edgeLength))
+    face.style.transform = getTransformForCubeFace(faceIndex, edgeLength)
   }
 
   container.style.top = percentString(topPct)
@@ -59,7 +59,119 @@ const createCubeParams = (edgeLength, leftPct, topPct) => {
 
 }
 
+const getTransformForRectangleFace = (faceIndex, width, height, depth) => {
+  const rotations = [
+    'rotateY(0deg)',
+    'rotateY(90deg)',
+    'rotateY(180deg)',
+    'rotateY(270deg)',
+    'rotateX(90deg)',
+    'rotateX(-90deg)']
+  const translations = [
+    depth/2,
+    0,
+    depth/2,
+    depth/2,
+    height,
+    -2*height
+  ]
+  return `${rotations[faceIndex]} translateZ(${translations[faceIndex]}em)`
+}
 
-for (let i =0; i < 3; i++) {
-  createCubeParams(4, randomInRange(3,93), randomInRange(3,93))
+const styleForFace = (faceIndex, width, height, depth) => {
+  const transform = getTransformForRectangleFace(faceIndex, width, height, depth)
+  const widthEm = emString(width)
+  const heightEm = emString(height)
+  const depthEm = emString(depth)
+  const sizings = [
+    {
+      width: widthEm,
+      height: heightEm,
+    }, {
+      width: depthEm,
+      height: heightEm,
+      left: emString(width-depth/2)
+    }, {
+      width: widthEm,
+      height: heightEm,
+    }, {
+      width: depthEm,
+      height: heightEm,
+    }, {
+      width: widthEm,
+      height: depthEm,
+      top: emString(depth*2)
+    }, {
+      width: widthEm,
+      height: depthEm,
+      top: emString(depth*2)
+    }
+  ]
+  return {
+    transform,
+    ...sizings[faceIndex]
+  }
+}
+
+const assignStyles = (element, styles) => {
+  for (style in styles) {
+    element.style[style] = styles[style]
+  }
+}
+
+const createRectangularPrismParams = (width, height, depth) => {
+  const id = counter()
+  // const widthLengthEm = emString(width)
+  // const widthLengthEm = emString(width)
+
+  const container = document.createElement('div')
+  container.id = `container-${id}`
+  const faces = []
+  // 0 - top
+  // 1 - right
+  // 2 - bottom
+  // 3 - left
+  // 4 - up
+  // 5 - down
+  for (let faceIndex = 0; faceIndex < 6; faceIndex ++) {
+    const face  = document.createElement('div')
+    face.id = `face-${faceIndex}-${id}`
+    container.appendChild(face)
+    // face.style.backgroundColor = 'black'
+    // face.style.margin= -0.5*edgeLengthEm;
+    // console.log(styleForFace(faceIndex, width, height, depth))
+    // face.style = {...face.style, ...styleForFace(faceIndex, width, height, depth)}
+    assignStyles(face, styleForFace(faceIndex,width, height, depth))
+    console.log(face.style)
+    // face.style.width = edgeLengthEm;
+    // face.style.height = edgeLengthEm;
+    face.style.position = 'absolute'
+    face.classList.add('wireframe')
+    
+    // console.log(getTransformForCubeFace(faceIndex, edgeLength))
+    // face.style.transform = getTransformForCubeFace(faceIndex, edgeLength)
+  }
+
+  // container.style.top = percentString(topPct)
+  // container.style.left = percentString(leftPct)
+  container.style.position ='absolute'
+  container.style.transformStyle = 'preserve-3d'
+  container.style.transform = `translateZ(${depth/2}em)`
+
+  return container
+
+}
+
+
+for (let i =0; i < 1; i++) {
+  // createCubeParams(randomInRange(1,10), randomInRange(3,93), randomInRange(3,93))
+  // const rect = createRectangularPrismParams(randomInRange(5,5), randomInRange(12,12), randomInRange(16,16))
+  const rect = createRectangularPrismParams(12, 20,16) 
+  // 12 width -> 4 left
+  // 17 width -> 9 left
+  // 3 width -> -5 left
+  // -> 
+  rect.style.top = percentString(randomInRange(50,50))
+  rect.style.left = percentString(randomInRange(30,30))
+  ground.appendChild(rect)
 }
